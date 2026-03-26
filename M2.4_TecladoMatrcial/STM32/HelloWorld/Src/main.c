baf18fefe93f1ffe9c646204f4e510629cb9691b
A01385688 <A01385688@tec.mx>
2026-03-25 23:51:42 -0600

    Update v1.A mainC comments

--- M2.4_TecladoMatrcial/STM32/HelloWorld/Src/main.c
+++ M2.4_TecladoMatrcial/STM32/HelloWorld/Src/main.c
@@ -7,18 +7,18 @@ void USER_Delay_1sec(void);
 
 int main(void)
 {
-	USER_GPIO_Init( ); 
-	USER_Keypad_Init( ); 
-	uint8_t tecla_presionada = 0xFFU;
-    for(;;){
-		tecla_presionada = USER_Key();
-    	if(tecla_presionada != 0xFFU){
-            GPIOA->ODR = (GPIOA->ODR & ~0xFUL) | (tecla_presionada & 0xFUL);
+	USER_GPIO_Init( );//														Se llama a la funcion GPIO_Init
+	USER_Keypad_Init( );//														Se llama a la funcion Keypad_Init
+	uint8_t tecla_presionada = 0xFFU;//											Variable para la tecla presionada
+    for(;;){//																	Bucle y/o Ciclo (Instrucciones que se repetiran)
+		tecla_presionada = USER_Key();//										Asignamos el valor devuelto por la funcion USER_key (Indica el boton presionado)
+    	if(tecla_presionada != 0xFFU){//										Si la tecla esta precionada entramos al if
+            GPIOA->ODR = (GPIOA->ODR & ~0xFUL) | (tecla_presionada & 0xFUL);//	Muestra el resultado en los primeros pines del puerto A 0 - 3 que corresponden a los LEDS
         }
     }
 }
 
-void USER_GPIO_Init( void ){
+void USER_GPIO_Init( void ){//								Reset AND Set de los PinA y la configuración de los mismos
 	RCC->APB2ENR |= ( 0x1UL << 2U );
 	GPIOA->ODR &= ~( 0x1UL << 0U ); 
     GPIOA->ODR &= ~( 0x1UL << 1U ); 
@@ -33,7 +33,7 @@ void USER_GPIO_Init( void ){
     GPIOA->CRL &= ~( 0x3UL << 14U ) & ~( 0x2UL << 12U );
     GPIOA->CRL |= ( 0x1UL << 12U );
 }
-void USER_Delay_1sec( void ){
+void USER_Delay_1sec( void ){//								Logica de la función Delay: Espera 1 segundo
 	__asm(" 		ldr r0, =888888	");
 	__asm(" again:	sub r0, r0, #1		");
 	__asm("			cmp r0, #0			");
